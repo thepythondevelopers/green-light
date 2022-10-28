476b33db00bdd42aaacb2c6b8529282886100da8
Pamsar <99356910+thepythondevelopers@users.noreply.github.com>
2022-10-28 20:20:57 +0530

    Code

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+
+//Routes
+
+//Connection
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true
+}).then(()=>{
+    console.log('DATA CONNECTED');
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+
+
+app.listen(port,()=>{
+    console.log(`Server is running at port ${port}`)
+});
+
