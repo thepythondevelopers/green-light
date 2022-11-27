3ded1a4b2fd78ffa9295daa9b6016715d720e0c0
Pamsar <99356910+thepythondevelopers@users.noreply.github.com>
2022-11-27 15:09:49 +0530

    Api Changes

--- controllers/light.js
+++ controllers/light.js
@@ -11,32 +11,70 @@ exports.saveLight = async (req,res)=>{
     initiated_id : req.user._id,
     sent_light : req.body.sent_light
   }
-  Light.findOneAndUpdate(
-          {sent_to : req.body.sent_to,initiated_id : req.user._id},
-          {$set : data},
-          {upsert: true,new: true},
-          (err,light) => {
-            if(err){
+  light = await Light.findOne({sent_to1 : req.user._id,user1: ObjectId(req.body.sent_to)});
+  
+  if(light!=null){
+    data = {
+      user2 : req.user._id,
+      sent_to2 : req.body.sent_to,
+      sent_light2 : req.body.sent_light
+    }
+    await Light.findOneAndUpdate(
+      {sent_to1 : req.user._id,user1: ObjectId(req.body.sent_to)},
+      {$set : data},
+      {new: true},
+      (err,u) => {
+          if(err){
+              return res.status(404).json({
+                  error : err
+              })
+          
+          }
+  
+          if(u===null){
+              return res.status(404).json({
+                  message : "No Data Found"
+              })
+          }
+  
+          return res.json({'message' : 'Light Send Successfully.'})
+      })
+  }else{
+    light_check = await Light.findOne({user1 : req.user._id,sent_to1: ObjectId(req.body.sent_to)});
+    if(light_check!=null){
+      return res.status(400).json({'message' : 'Already Light Send.'})
+    }
+    data = {
+      user1 : req.user._id,
+      sent_to1 : req.body.sent_to,
+      sent_light1 : req.body.sent_light
+    }
+  
+  l =new Light(data);
+  l.save((err,li)=>{
+        if(err){
             return res.status(400).json({
-                message : "Something Went Wrong"
+                message : err
             })
         }
-        return res.json(light);
+        return res.json({'message' : 'Light Send Successfully.'})
   })
+}
+  
 }
 
 exports.sentGreenLight = async (req,res)=>{
-  user = await Light.find({initiated_id: req.user._id,sent_light : 'Green'}).populate('initiated_id','display_name').populate('sent_to','display_name');
+  user = await Light.find({user1: req.user._id,sent_light1 : 'Green'}).populate('user1','display_name').populate('sent_to1','display_name');
   return res.send(user);
 }
 
 exports.yellowLight = async (req,res)=>{
-  user = await Light.find({initiated_id: req.user._id,sent_light : 'Yellow'}).populate('initiated_id','display_name').populate('sent_to','display_name');
+  user = await Light.find({user1: req.user._id,sent_light1 : 'Yellow'}).populate('user1','display_name').populate('sent_to1','display_name');
   return res.send(user);
 }
 
 exports.mutualGreenLight = async (req,res)=>{
-  user = await Light.find({$or: [ { initiated_id:  ObjectId(req.user._id)  }, { sent_to: ObjectId(req.user._id) } ],sent_light:"Green",response_light: "Green"}).populate('initiated_id','display_name').populate('sent_to','display_name');
+  user = await Light.find({$or: [ { user1:  ObjectId(req.user._id)  }, { sent_to1: ObjectId(req.user._id) } ],sent_light1:"Green",sent_light2: "Green"}).populate('initiated_id','display_name').populate('sent_to','display_name');
   return res.send(user);
 }
 
